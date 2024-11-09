3ed18987caa2a66d5b14523ba78c69605d53c598
Filippo Contessa <filippo.contessa@studio.unibo.it>
2024-11-09 18:12:30 +0100

    implemented a way to export the nifti images

--- nifti_image_loader.py
+++ nifti_image_loader.py
@@ -1,5 +1,6 @@
 import os
-import nibabel as nib  # per gestire i file .nii in Python
+import nibabel as nib
+import pickle
 
 # Percorso principale contenente le cartelle sub-versexxx
 main_path = "/mnt/d/osfstorage-archive/dataset-verse19test/derivatives"
@@ -8,20 +9,19 @@ main_path = "/mnt/d/osfstorage-archive/dataset-verse19test/derivatives"
 nii_images = []
 image_names = []
 
-# Itera attraverso le sottocartelle
+# Itera attraverso le sottocartelle per caricare le immagini
 for subdir in os.listdir(main_path):
     subdir_path = os.path.join(main_path, subdir)
-    
-    # Controlla se il percorso subdir_path sia una directory con il prefisso corretto
     if os.path.isdir(subdir_path) and subdir.startswith("sub-verse"):
-        # Cerca il file .nii nella sottocartella
-        for file in os.listdir(subdir_path):  # Scorre tutti i file nella subdir_path
+        for file in os.listdir(subdir_path):
             if file.endswith(".nii"):
                 nii_path = os.path.join(subdir_path, file)
-                
-                # Carica il file .nii e aggiungilo alla lista
                 nii_image = nib.load(nii_path)
                 nii_images.append(nii_image)
-                image_names.append(file)  # Salva il nome del file
+                image_names.append(file)
 
-# Ora nii_images contiene tutte le immagini caricate e image_names i loro nomi
+# Salva le immagini e i nomi in un file .pkl
+with open("images_data.pkl", "wb") as f:
+    pickle.dump((nii_images, image_names), f)
+
+print(f"Caricate e salvate {len(nii_images)} immagini .nii")
