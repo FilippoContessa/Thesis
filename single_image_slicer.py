6b58736e91d2c45dabefd4d7bc1a33c3f99074ab
Filippo Contessa <filippo.contessa@studio.unibo.it>
2024-11-10 11:46:48 +0100

    implemented a function-based way

--- single_image_slicer.py
+++ single_image_slicer.py
@@ -6,30 +6,33 @@ import matplotlib.pyplot as plt
 with open("images_data.pkl", "rb") as f:
     nii_images, image_names = pickle.load(f)
 
-index = 12
+def get_slices(index, nii_images, image_names):
 
-selected_image = nii_images[index]
-selected_image_name = image_names[index]
+    selected_image = nii_images[index]
+    selected_image_name = image_names[index]
 
-# Ottieni i dati dell'immagine
-image_data = selected_image.get_fdata()
-print(f"Nome dell'immagine selezionata: {selected_image_name}")
-print(f"Forma dell'immagine: {image_data.shape}")
+    # Ottieni i dati dell'immagine
+    image_data = selected_image.get_fdata()
+    print(f"Nome dell'immagine selezionata: {selected_image_name}")
+    print(f"Forma dell'immagine: {image_data.shape}")
 
-# Seleziona tre slice a diverse profondità:
+    # Seleziona tre slice a diverse profondità:
 
-# Slice verticali:
-x_quarter = image_data.shape[0]//4
-x_center = image_data.shape[0]//2
-x_three_quarters = (image_data.shape[0] * 3) // 4
+    # Slice verticali:
+    x_quarter = image_data.shape[0]//4
+    x_center = image_data.shape[0]//2
+    x_three_quarters = (image_data.shape[0] * 3) // 4
 
-sagittal_slice_quarter = image_data[x_quarter, :, :]
-sagittal_slice = image_data[x_center, :,:]  
-sagittal_slice_three_quarters = image_data[x_three_quarters, :, :]
+    sagittal_slice_quarter = image_data[x_quarter, :, :]
+    sagittal_slice = image_data[x_center, :,:]  
+    sagittal_slice_three_quarters = image_data[x_three_quarters, :, :]
 
+    slices = [sagittal_slice_quarter, sagittal_slice, sagittal_slice_three_quarters]
+    return slices
 
+def plot_slices(slice):
+    plt.imshow(slice, cmap="gray")
+    plt.show()
 
-# Visualizza le slice
-plt.imshow(sagittal_slice, cmap="gray")
-plt.title(f"Slice 1 di {selected_image_name}")
-plt.show()
+slices = get_slices(35, nii_images, image_names)
+plot_slices(slices[1])
