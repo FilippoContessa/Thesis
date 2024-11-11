729c3ede74c4d810a8f3bc9df5eebdf628f13244
Filippo Contessa <filippo.contessa@studio.unibo.it>
2024-11-11 12:44:17 +0100

    extended for a number N of image

--- total_slice_maker.py
+++ total_slice_maker.py
@@ -5,5 +5,9 @@ import pickle
 with open("images_data.pkl", "rb") as f:
     nii_images, image_names = pickle.load(f)
 
-    slices = get_slices(0, nii_images, image_names)
-    save_slices(slices)
+for i in range(31,len(nii_images)): 
+    slices = get_slices(i, nii_images, image_names)
+    save_slices(slices,image_names[i])
+
+
+#mancano all'appello 108,119,236
