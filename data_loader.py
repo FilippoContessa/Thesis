a90ca39bdd3e2f172605a266f0e69f58ca223122
Filippo Contessa <filippo.contessa@studio.unibo.it>
2024-11-12 16:27:18 +0100

    Extended functionality to .json files. Renaming

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,12 @@
 import os
 import nibabel as nib
+import json
 import pickle
 
 # Percorso principale contenente le cartelle sub-versexxx
 main_path = "/mnt/d/osfstorage-archive/dataset-verse19test/derivatives"
+
+# Caricamento delle immagini con nomi file:
 
 # Liste per memorizzare le immagini caricate e i nomi dei file
 nii_images = []
@@ -25,3 +28,28 @@ with open("images_data.pkl", "wb") as f:
     pickle.dump((nii_images, image_names), f)
 
 print(f"Caricate e salvate {len(nii_images)} immagini .nii")
+
+
+# Caricamento dei dati JSON e salvataggio in .pkl
+
+json_info = []
+
+for subdir in os.listdir(main_path):
+    subdir_path = os.path.join(main_path, subdir)
+    if os.path.isdir(subdir_path) and subdir.startswith("sub-verse"):
+        for file in os.listdir(subdir_path):
+            if file.endswith(".json"):
+                json_path = os.path.join(subdir_path, file)
+                
+                # Carica il file JSON
+                with open(json_path, 'r') as f:
+                    data = json.load(f)
+                    json_info.append(data)
+
+# Salva l'intera lista `json_info` in un file .pkl
+pkl_path = os.path.join(main_path, "combined_data.pkl")
+with open("json_data.pkl", 'wb') as pkl_file:
+    pickle.dump(json_info, pkl_file)
+
+print(f"Caricate e salvate {len(json_info)}  file JSON")
+
