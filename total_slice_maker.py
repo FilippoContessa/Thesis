56ab4b57eddff32a11a21e8ffdd75b9cefb2a982
Filippo Contessa <filippo.contessa@studio.unibo.it>
2024-11-12 17:15:39 +0100

    prepare to include .json info

--- total_slice_maker.py
+++ total_slice_maker.py
@@ -1,12 +1,15 @@
 from single_image_slicer import get_slices, plot_slice, save_slices
 import pickle
 
-# Carica le immagini e i nomi dal file .pkl
+# Carica le immagini e i nomi e le info .json dai file .pkl 
 with open("images_data.pkl", "rb") as f:
     nii_images, image_names = pickle.load(f)
 
+with open("json_data.pkl", "rb") as f:
+    json_files = pickle.load(f)
+
 # FIXME: mancano all'appello 108,119,236 + il ciclo for che sembra arrestarsi sempre alla 108/119
 
-for i in range(31,len(nii_images)): 
-    slices = get_slices(i, nii_images, image_names)
+for i in range(0,1): 
+    slices = get_slices(i, nii_images, image_names, json_files)
     save_slices(slices,image_names[i])
