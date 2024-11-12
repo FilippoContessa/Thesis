98b4fe5450ef7fcb1ae5365ecde8e93b635d863b
Filippo Contessa <filippo.contessa@studio.unibo.it>
2024-11-12 16:30:09 +0100

    Renaming

--- total_slice_maker.py
+++ total_slice_maker.py
@@ -5,9 +5,8 @@ import pickle
 with open("images_data.pkl", "rb") as f:
     nii_images, image_names = pickle.load(f)
 
+# FIXME: mancano all'appello 108,119,236 + il ciclo for che sembra arrestarsi sempre alla 108/119
+
 for i in range(31,len(nii_images)): 
     slices = get_slices(i, nii_images, image_names)
     save_slices(slices,image_names[i])
-
-
-# FIXME: mancano all'appello 108,119,236
