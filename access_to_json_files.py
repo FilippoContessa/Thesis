735742bfbe7ec1da6a3346b9df9a7ee04390922b
Filippo Contessa <filippo.contessa@studio.unibo.it>
2024-11-13 17:32:08 +0100

    A step ahead

--- access_to_json_files.py
+++ access_to_json_files.py
@@ -4,14 +4,40 @@ import pickle
 with open("json_data.pkl", "rb") as f:
     json_files = pickle.load(f)
 
-# Carica il primo file JSON
-json_info = json_files[0]
 
-# Accede alla coordinata X,Y,Z della vertebra con label 17
-#L'indice 1 identifica la vertebra che sto analizzando
-vertebrae_index = 1
-X , Y, Z  = json_info[vertebrae_index].get("X"), json_info[vertebrae_index].get("Y"), json_info[vertebrae_index].get("Z")  # Il secondo elemento ha la coordinata X pari a 94.8
+def get_vertebrae_coordinates(json_file_index, json_files, vertebrae_index):
+    json_file = json_files[json_file_index]
+    coordinates = []
+    X , Y, Z  = json_file[vertebrae_index].get("X"), json_file[vertebrae_index].get("Y"), json_file[vertebrae_index].get("Z")  # Il secondo elemento ha la coordinata X pari a 94.8
 
-print(f"Il valore della coordinata X è: {X}")
-print(f"Il valore della coordinata Y è: {Y}")
-print(f"Il valore della coordinata Z è: {Z}")
+    return X, Y, Z
+
+coordinates = get_vertebrae_coordinates(0,json_files,6)
+
+print(coordinates[0], coordinates[1], coordinates[2])
+
+def get_vertebrae_distances(index, json_files):
+    json_info = json_files[index]
+    coordinate_differences = []
+    
+    for i in range(1, len(json_info)-1):  # Ignora il primo e l'ultimo elemento per evitare errori
+        x_current = json_info[i].get("X")
+        y_current = json_info[i].get("Y")
+        z_current = json_info[i].get("Z")
+        
+        x_next = json_info[i + 1].get("X")
+        y_next = json_info[i + 1].get("Y")
+        z_next = json_info[i + 1].get("Z")
+    
+        if x_current is not None and x_next is not None and y_current is not None and y_next is not None and z_current is not None and z_next is not None:
+            # Calcola la differenza assoluta approssimando al valore intero più vicino per ciascuna dimensione
+            diff_x = round(abs(x_next - x_current) / 2)
+            diff_y = round(abs(y_next - y_current) / 2)
+            diff_z = round(abs(z_next - z_current) / 2)
+            
+            label_current = json_info[i].get("label")
+            label_next = json_info[i + 1].get("label")
+            
+            coordinate_differences.append((label_current, label_next, diff_x, diff_y, diff_z))
+
+    return coordinate_differences
--- single_image_slicer.py
+++ single_image_slicer.py
@@ -2,31 +2,6 @@ import pickle
 import os
 import matplotlib.pyplot as plt
 
-def get_veretbrae_distances(index, json_files):
-    json_info = json_files[index]
-    coordinate_differences = []
-    
-    for i in range(1, len(json_info)-1):  # Ignora il primo e l'ultimo elemento per evitare errori
-        x_current = json_info[i].get("X")
-        y_current = json_info[i].get("Y")
-        z_current = json_info[i].get("Z")
-        
-        x_next = json_info[i + 1].get("X")
-        y_next = json_info[i + 1].get("Y")
-        z_next = json_info[i + 1].get("Z")
-    
-        if x_current is not None and x_next is not None and y_current is not None and y_next is not None and z_current is not None and z_next is not None:
-            # Calcola la differenza assoluta approssimando al valore intero più vicino per ciascuna dimensione
-            diff_x = round(abs(x_next - x_current) / 2)
-            diff_y = round(abs(y_next - y_current) / 2)
-            diff_z = round(abs(z_next - z_current) / 2)
-            
-            label_current = json_info[i].get("label")
-            label_next = json_info[i + 1].get("label")
-            
-            coordinate_differences.append((label_current, label_next, diff_x, diff_y, diff_z))
-
-    return coordinate_differences
 
 def get_slices(index, nii_images, image_names, json_files):
     selected_image = nii_images[index]
